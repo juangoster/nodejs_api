eb8cbad43fce1bb4889781aa5e09f8ac71cbf0d7
Juan Guillermo Zapata Grajales <jzgrajal@bancolombia.com.co>
2022-09-02 16:20:35 -0500

    se crea el server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello nodejs')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+  res.send('hola este es mi server nuevo en express')
+})
+app.get('/products', (req, res)=>{
+  res.json({
+    name:"café",
+    price: 15000
+  })
+})
+
+app.listen(port, ()=>{
+  console.log('server corriendo en el puerto: '+ port)
+})
+
